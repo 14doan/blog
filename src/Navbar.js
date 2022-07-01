ffc5ea9573bb20cb99b1496dffb6fb31fae969ba
14doan <82048728+14doan@users.noreply.github.com>
2022-07-01 07:00:41 +0100

    nav

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Ways Of Survival</h1>
+            <div className="links">
+                <a href="/home">Home</a>
+                <a href="/create">Create Content</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar; 
