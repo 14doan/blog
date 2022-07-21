abbd261fa2ba0077094aa5cebd2045ba5f60af5a
14doan <82048728+14doan@users.noreply.github.com>
2022-07-21 07:13:29 +0100

    21/7

--- src/Home.js
+++ src/Home.js
@@ -4,25 +4,38 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const[blogs,setBlogs]=useState(null);
+    const[isPending,setIsPending]=useState(true);
+    const[error,setError]=useState(null);
      
-    const deleteClick=(id)=>{
-        const newBlogs=blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
    
     useEffect(()=>{
+        setTimeout(()=>{
        fetch('http://localhost:8000/blogs')
        .then((response => {
+        if(!response.ok){
+          throw Error ("computer says no");
+        }
+        console.log(response);
         return response.json();
        }))
        .then((da)=>{
+        console.log(da);
+        setError(null);
+        setIsPending(false);
         setBlogs(da);
-       }
-       ) 
+        
+       })
+       .catch(err=>{
+        setError(err.message);
+        setIsPending(false);
+       })
+       },1000) 
     },[])
     return ( 
         <div className="home">
-            {blogs && <BlogList propBlogs={blogs} title="All blogs" del={deleteClick}/>}
+            {error && <div>{error}</div>}
+            {isPending && <div> loading...</div>}
+            {blogs && <BlogList propBlogs={blogs} title="All blogs" />}
         </div>
      );
 }
