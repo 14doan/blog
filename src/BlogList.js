389c139b949bd7fa2556dbbe2f5a38033c0b06a4
14doan <82048728+14doan@users.noreply.github.com>
2022-07-29 07:42:14 +0100

    create

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({propBlogs ,title}) => {
     return ( 
         <div className="blog-list">
             <h3>{title}</h3>
             {propBlogs.map((b)=>(
                 <div className="blog-preview" key={b.id}>
-                    <h3>{b.title}</h3>
-                    <p>Written by {b.author}</p>
+                    <Link to= {`/blogs/${b.id}`}>
+                        <h3>{b.title}</h3>
+                        <p>Written by {b.author}</p>
+                    </Link>
+                    
                 </div>
             ))}
         </div>
--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,45 @@
+import { toBeInTheDocument } from "@testing-library/jest-dom/dist/matchers";
+import { useState } from "react";
+
 const Create = () => {
+    const[title,setTitle]=useState('');
+    const[body, setBody]=useState('');
+    const[author, setAuthor]=useState('s0');
+
     return ( 
-        <div>
-            <h1>Add New Blog</h1>
+        <div className="create">
+            <h2>Add New Blog</h2>
+            <form>
+                <label>Title</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e)=>setTitle(e.target.value)}
+                />
+    
+                <label>Body</label>
+               <textarea
+                    required
+                    value={body}
+                    onChange={(e)=>setBody(e.target.value)}
+               />
+
+                <label>Author</label>
+                <select
+                required
+                value={author}
+                onChange={(e)=>setAuthor(e.target.value)}
+                >
+                    <option value="s0">S0</option>
+                    <option value="s1">s1</option>
+                </select>
+                <button>Add blog</button>
+            </form>
         </div>
+        
      );
 }
+
  
 export default Create;
