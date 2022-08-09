aaea7118ab56cc5e10ffe8e1e0b587f83801a788
14doan <82048728+14doan@users.noreply.github.com>
2022-08-09 22:17:14 +0100

    dfdg

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route exact path="/">
+            <Route path="/">
               <Home />
             </Route>
             <Route path="/create">
